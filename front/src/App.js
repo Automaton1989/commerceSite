d6062e8b0ffb9ef29b5217ae972dde1fe2cce52f
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2021-11-16 13:56:54 -0800

    added proper react linking

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Login";
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className = "App">
         <h1>HOME Here</h1>
+        <Link to="/login">Login</Link>
         <Routes>
           <Route path="/login" element={<Login />}/>
         </Routes>
