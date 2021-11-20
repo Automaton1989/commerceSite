de301aa192cc23180b1468cc067449d1c152458b
jenniferxsj <xiao.57578@gmail.com>
2021-11-19 19:04:21 -0800

    Implement the home page

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {Carousel} from "react-bootstrap";
+import petStuff1 from "../images/pet-stuff.jpg";
+import petStuff2 from "../images/pet-stuff2.jpg";
+import petStuff3 from "../images/pet-stuff3.jpg";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img src={petStuff2} alt="" className="d-block w-100" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={petStuff3} alt="" className="d-block w-100" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={petStuff1} alt="" className="d-block w-100" />
+        </Carousel.Item>
+      </Carousel>
+      <div className="col align-self-center">
+        <h1>Welcome to the Store!</h1>
+        <p>You can find everything you need here for your pets!</p>
+        <Link to="/login"><button type="button" className="btn btn-color btn-primary btn-lg">Start Shooping</button></Link>
+      </div>
     </div>
   );
 }
