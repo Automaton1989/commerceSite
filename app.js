0255ef08c224f0671eb794851579b0c7f3b3b167
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2021-11-24 14:32:53 -0800

    adjusted app js again for deployment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 if(process.env.NODE_ENV === "production") {
-	app.use(express.static("front/build"));
+	app.use(express.static(path.join(__dirname, "front", "build")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "front", "build", "index.html"));
